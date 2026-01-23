a8cc6d9ec7a88a9d76eebe23e4630ea41be03349
syedtufailasuspro <syedtufailmipro@gmail.com>
2026-01-23 17:19:51 +0530

    Added SignUp Page

--- clashofcode/Users/urls.py
+++ clashofcode/Users/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("login_user", views.login_user, name= "login"),
+    path("signup_user", views.signup_user, name= "signup"),
 ]
--- clashofcode/Users/views.py
+++ clashofcode/Users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
 
 def login_user(request):
     if request.method == 'POST':
@@ -19,3 +19,31 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'authentication/login.html', {})
+
+
+
+
+def signup_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already taken')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already registered')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                messages.success(request, 'Account created successfully!')
+                return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('signup')
+    else:
+        return render(request, 'authentication/signup.html', {})
