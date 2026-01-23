fdc273ac65d7f5daacea83d644b4005bf57bae7e
syedtufailasuspro <syedtufailmipro@gmail.com>
2026-01-23 16:42:38 +0530

    Created the login page UI and the functionality

--- clashofcode/Users/views.py
+++ clashofcode/Users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-# Create your views here.
+@csrf_exempt
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            return HttpResponse("WELCOME CHIEF")
+            ...
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(request, ('ERROR'))
+            return redirect('login')
+    else:
+        return render(request, 'authentication/login.html', {})
--- clashofcode/clashofcode/urls.py
+++ clashofcode/clashofcode/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("django.contrib.auth.urls")),
     path("users/", include("Users.urls")),
 ]
+
+
